154a5258a53d1ef51b7e44f8cd5dfb6abe551179
rtritto <ariele.1994@gmail.com>
2024-12-08 21:42:33 +0100

    Remove deps and route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,38 +1,10 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-// import { logger } from 'hono/logger'
-import { autoloadRoutes } from 'hono-autoloader'
 import vike from 'vike-node/hono'
 
 const port = +(process.env.PORT || 3000)
 
 const app = new Hono()
-
-app.use(cors())
-
-// app.use(logger())
-
-let unregister
-if (process.env.NODE_ENV === 'production') {
-  unregister = () => { }
-} else {
-  const { register } = await import('tsx/esm/api')
-  // Register tsx enhancement
-  unregister = register()
-}
-
-await autoloadRoutes(
-  app,
-  {
-    pattern: '**/*.ts',
-    prefix: '/api',
-    routesDir: './src/server/api'
-  }
-)
-
-// Unregister when needed
-unregister()
 
 app.use(vike())
 
