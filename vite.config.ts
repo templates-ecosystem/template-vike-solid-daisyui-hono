54e31047b6d944aaeda042a2378f00f8c71d0104
rtritto <ariele.1994@gmail.com>
2024-12-18 21:30:34 +0100

    Repro React is not defined

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import path from 'node:path'
 import { plugin as vike } from 'vike/plugin'
-import { vikeNode } from 'vike-node/plugin'
-import vikeSolid from 'vike-solid/vite'
 import type { UserConfig } from 'vite'
 
 export default {
   root: 'src',
   cacheDir: path.resolve(import.meta.dirname, '.vite'),
   plugins: [
-    vike(),
-    vikeNode('server/index.ts'),
-    vikeSolid()
+    vike()
   ],
   server: {
     port: 3000
