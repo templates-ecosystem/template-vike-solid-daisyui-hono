fa8b655d5d19a765f409dc12306c6c3053d61df1
rtritto <ariele.1994@gmail.com>
2025-01-11 18:34:27 +0100

    feat: add universal-autorouter-hono

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,18 +1,33 @@
+import path from 'node:path'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 // import { logger } from 'hono/logger'
-import autoloadRoutes from 'universal-autorouter'
+import type { AutoloadRoutesOptions } from 'universal-autorouter'
 import vike from 'vike-node/hono'
 
-const app = await autoloadRoutes(new Hono(), {
-  pattern: process.env.NODE_ENV === 'production' ? '**/*.mjs' : '**/*.ts',
-  prefix: '/api',
-  routesDir: './src/server/api',
-  viteDevServer: globalThis.__vikeNode?.viteDevServer
-})
+const app = new Hono()
 
 app.use(cors())
+
+let autoloadRoutes
+// Options of "universal-autorouter" package
+const autoloadRoutesOptions = {
+  prefix: '/api',
+  routesDir: path.resolve(import.meta.dirname, 'api')
+} as Omit<AutoloadRoutesOptions, 'viteDevServer'> & {
+  viteDevServer: import('vite').ViteDevServer
+}
+if (process.env.NODE_ENV === 'production') {
+  ({ default: autoloadRoutes } = await import('universal-autorouter'))
+  autoloadRoutesOptions.pattern = '**/*.mjs'
+} else {
+  ({ default: autoloadRoutes } = await import('universal-autorouter-hono'))
+  autoloadRoutesOptions.pattern = '**/*.ts'
+  autoloadRoutesOptions.viteDevServer = globalThis.__vikeNode!.viteDevServer
+}
+
+await autoloadRoutes(app, autoloadRoutesOptions as unknown as AutoloadRoutesOptions)
 
 // app.use(logger())
 
