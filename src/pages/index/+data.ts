010d2b046b1996922c2024e920b51e647709d869
rtritto <ariele.1994@gmail.com>
2025-02-15 18:37:36 +0100

    Add log pageContext.urlOriginal

--- src/pages/index/+data.ts
+++ src/pages/index/+data.ts
@@ -1,6 +1,7 @@
 import type { DataAsync } from 'vike/types'
 
 export const data: DataAsync = async (pageContext) => {
+  console.log('pageContext.urlOriginal: ', pageContext.urlOriginal)
   return {
     urlOriginal: pageContext.urlOriginal
   }
