94b9ee227221778c4054ce4e72fdec6f6654d9cc
rtritto <ariele.1994@gmail.com>
2025-03-15 11:17:24 +0100

    Init vlite

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -1,6 +1,26 @@
+import 'vlitejs/vlite.css'
+import { createSignal, onCleanup, onMount } from 'solid-js'
+import Vlitejs from 'vlitejs'
+
 function Page() {
+  const [videoRef, setVideoRef] = createSignal<HTMLVideoElement>()
+
+  onMount(() => {
+    if (videoRef()) {
+      const player = new Vlitejs(videoRef()!, {
+        options: {
+          autoHide: true
+        }
+      })
+
+      onCleanup(() => {
+        player.destroy()
+      })
+    }
+  })
+
   return (
-    <>Hello World</>
+    <video ref={setVideoRef} class="vlite-js" src="https://yoriiis.github.io/cdn/static/vlitejs/demo-video-html5.mp4" />
   )
 }
 
