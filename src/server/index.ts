4bf4f49f6bb3d108571988a9506e9add1269d297
rtritto <ariele.1994@gmail.com>
2025-03-26 22:58:04 +0100

    chore(deps): replace @hono/node-server with vike-server/hono/serve

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,8 @@
-import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 // import { logger } from 'hono/logger'
 import { apply } from 'vike-server/hono'
+import { serve } from 'vike-server/hono/serve'
 
 import { handlerApi } from './handlers/handlerApi'
 
@@ -18,7 +18,4 @@ apply(app)
 
 const port = +(process.env.PORT || 3000)
 
-serve({
-  fetch: app.fetch,
-  port
-}, () => console.log(`Server running at http://localhost:${port}`))
+serve(app, { port })
