bb99afbe6aafcc0baa268a756f56b44af8a5f08b
rtritto <ariele.1994@gmail.com>
2026-04-20 21:37:56 +0200

    Debug

--- api/api.js
+++ api/api.js
@@ -1,4 +1,8 @@
 import { app } from '../dist/server/entrypoint.mjs'
 
+console.log('App loaded:', typeof app)
+
+console.log('App.fetch:', typeof app?.fetch)
+
 export const GET = app.fetch
 export const POST = app.fetch
