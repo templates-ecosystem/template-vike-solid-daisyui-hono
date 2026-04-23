492c4dc9f4603ae31fec3eeee7d5aaea4336a84a
rtritto <ariele.1994@gmail.com>
2026-04-23 15:00:03 +0200

    Logs

--- postBuild.js
+++ postBuild.js
@@ -6,13 +6,17 @@ const distFilePath = path.resolve('dist')
 // Move
 // - /dist/server/entrypoint.mjs → /dist/server/index.mjs
 fs.renameSync(path.join(distFilePath, 'server', 'entrypoint.mjs'), path.join(distFilePath, 'server', 'index.mjs'))
+console.log('✅ Renamed: /dist/server/entrypoint.mjs → /dist/server/index.mjs')
 
 // Remove
 // - /dist/server/chunks
 // - /dist/server/entry.mjs
 fs.rmSync(path.join(distFilePath, 'server', 'chunks'), { recursive: true, force: true })
+console.log('✅ Removed: /dist/server/chunks')
 fs.unlinkSync(path.join(distFilePath, 'server', 'entry.mjs'))
+console.log('✅ Removed: /dist/server/entry.mjs')
 
 // Debug
 // - Copy server from dist/server to dist/client/server
 fs.cpSync(path.join(distFilePath, 'server'), path.join(distFilePath, 'client', 'server'), { recursive: true })
+console.log('✅ Copied: /dist/server → /dist/client/server')
