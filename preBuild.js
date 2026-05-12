040fecd9d57bf51395aef4805dab7120ddf0e7db
rtritto <ariele.1994@gmail.com>
2026-05-12 20:56:12 +0200

    Revert "Add preBuild"
    This reverts commit ffbabad885ebfdbdcf463f6df7291c63b9fa0d6c.

--- preBuild.js
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs'
-import path from 'node:path'
-
-const distFilePath = path.resolve('src')
-
-// Remove
-fs.unlinkSync(path.join(distFilePath, 'pages', '+server.ts'))
-console.log('✅ Removed: /dist/pages/+server.ts')
