08d68c066892a0fedf662951aa46fdf68c25211c
rtritto <ariele.1994@gmail.com>
2026-05-17 21:38:44 +0200

    Use depatcher

---
+++ scripts/patcher.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import { execSync } from 'node:child_process'
+import { applyPatchMultiple } from 'depatcher'
+
+console.log('⏳ Applying patches …')
+
+await applyPatchMultiple({
+  '@universal-deploy/node': {
+    '/dist/vite.js': './scripts/patches/@universal-deploy__node_vite.patch'
+  },
+  vike: {
+    '/dist/node/vite/plugins/pluginUniversalDeploy.js': './scripts/patches/vike_pluginUniversalDeploy.patch'
+  }
+})
+
+console.log('✅ Patches applied!')
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import devServer from '@hono/vite-dev-server'
 import standaloner from 'standaloner/vite'
 import { plugin as vike } from 'vike/plugin'
 import vikeSolid from 'vike-solid/vite'
-import type { Plugin, UserConfig } from 'vite'
+import type { UserConfig } from 'vite'
 
 const { NODE_ENV, PORT } = process.env
 
@@ -17,36 +16,8 @@ export default {
       standaloner({
         bundle: true,
         minify
-      }),
-      {
-        name: 'emit-server-entrypoint',
-        apply: 'build',
-        enforce: 'post',
-        config() {
-          return {
-            environments: {
-              ssr: {
-                resolve: {
-                  noExternal: true
-                },
-                build: {
-                  rolldownOptions: {
-                    input: {
-                      index: 'server/entrypoint.ts'
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      } as Plugin
-    ] : [devServer({
-      entry: 'server/dev-entrypoint.ts',
-      // Can conflit the Vike's HMR, so avoid injecting the default Vite client script
-      // since Hono's dev server already handles HMR and live reload functionality
-      injectClientScript: false
-    })],
+      })
+    ] : [],
     vike(),
     vikeSolid()
   ],
