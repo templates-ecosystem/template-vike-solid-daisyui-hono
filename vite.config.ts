ba16ba0eaf231a470934b339947cb0941a1671d7
rtritto <ariele.1994@gmail.com>
2026-06-08 17:56:10 +0200

    clean build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import standaloner from 'standaloner/vite'
 import { plugin as vike } from 'vike/plugin'
 import vikeSolid from 'vike-solid/vite'
 import type { UserConfig } from 'vite'
 
-const { NODE_ENV, PORT } = process.env
+const { PORT } = process.env
 
 const port = PORT ? Number.parseInt(PORT) : 3000
 const minify = false
@@ -12,12 +11,6 @@ export default {
   root: 'src',
   cacheDir: '../.vite',
   plugins: [
-    ...NODE_ENV === 'production' ? [
-      standaloner({
-        bundle: true,
-        minify
-      })
-    ] : [],
     vike(),
     vikeSolid()
   ],
